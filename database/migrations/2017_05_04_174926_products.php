0edc6d14eb13aa3c068ded016a4166342fafeaad
theClubhou.se <heythere@theclubhou.se>
2017-05-04 15:45:58 -0400

    SRL Controllers

---
+++ database/migrations/2017_05_04_174926_products.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+        $table->increments('id');
+        $table->longText('image');
+        $table->integer('price');
+        $table->string('description');
+        $table->integer('quantity');
+        $table->string('name');
+        $table->integer('categoryID');
+        $table->boolean('availability');
+        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2017_05_04_175157_orders.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Orders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('orders', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('userID');
+          $table->integer('quantity');
+          $table->integer('productID');
+          $table->integer('totalPrice');
+          $table->string('comments');
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
