58eeaafe81d4037f52e49aed0e16be61ced4f1ba
theClubhou.se <heythere@theclubhou.se>
2017-05-08 10:18:02 -0400

    RolesController

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -6,11 +6,14 @@ use Illuminate\Http\Request;
 
 class RolesController extends Controller
 {
+
+
   public function index()
   {
     $roles = Roles::all();
     return Response::json($roles);
   }
+
 
   public function store (Request $request)
   {
@@ -19,4 +22,41 @@ class RolesController extends Controller
       "email" => "required",
       "password" => "required"
     ];
+  $validator = Validator::make(Purifier::clean($request->all()), $rules);
+
+  if($validator->fails())
+  {
+    return Response::json(["error"=>"please fill out all of the fields"]);
   }
+
+  $user = Auth::user();
+  if($user->roleID != 1)
+  {
+    return Response::json(["error" => "You can't enter here."]);
+  }
+}
+
+
+  public function update($id, Request $request)
+  {
+    $role = Role::find($id);
+    $role->title = $request->input('role');
+    $role->save();
+    return Response::json(["success"=>"Role Updated."]);
+  }
+
+
+  public function show($id)
+  {
+    $role = Article::find($id);
+    return Response::json($role);
+  }
+
+
+  public function destroy($id)
+  {
+    $role = Role::find($id);
+    $role->delete();
+    return Response::json(['success' => 'Role deleted.']);
+  }
+}
